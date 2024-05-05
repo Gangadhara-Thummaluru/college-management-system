1fea70b1c9a49a568a61f5c10e50984159b7fb94
Gangadhara-Thummaluru <thummalurugangadhar7@gmail.com>
2024-05-05 15:56:34 +0530

    college-management-system project changes

---
+++ home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/home.css"/>
+<title>College management system </title>
+</head>
+
+<body>
+	<div class="panel panel-default">
+  		<div class="panel-heading"><h1>College Management System</h1></div>
+                <div class="panel-body"><br><br>
+        <div>
+            <img src="images/rgukt_image.png" width="855" style="text-align: center;" height="308" onmousedown="return false;"  />
+        </div><br><br>
+        <div class="middle_text">
+            <p><strong>Rajiv Gandhi University of Knowledge Technologies (RGUKT) is an autonomous university located in Idupulapaya in the state of Andhra Pradesh, India. The university was established in 2008.</strong></p>
+        </div>
+                
+        <div class="text_para_home">
+            <p>In March of 2008, the AP Government created RGUKT with three campuses through an Act of the Legislature as a full-fledged university which would initially admit approximately the top 1% of the rural students into the three residential campuses located at RK Valley, Basara and Nuzvid.</p>         
+        </div>   
+                </div>
+        </div>
+    
+    
+</body>
+</html>
